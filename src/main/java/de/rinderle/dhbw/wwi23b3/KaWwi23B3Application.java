e646f44ec5f88c56a6128fcddd67a8f4270f4b5b
linamarieotte <109418061+linamarieotte@users.noreply.github.com>
2024-10-14 11:03:18 +0200

    123

--- src/main/java/de/rinderle/dhbw/wwi23b3/KaWwi23B3Application.java
+++ src/main/java/de/rinderle/dhbw/wwi23b3/KaWwi23B3Application.java
@@ -3,6 +3,12 @@ package de.rinderle.dhbw.wwi23b3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+
+
+
 @SpringBootApplication
 public class KaWwi23B3Application {
 
